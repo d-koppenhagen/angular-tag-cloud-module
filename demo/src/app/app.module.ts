59e8502b75c0b400a5228046bc8f7effd374888b
Danny Koppenhagen <mail@d-koppenhagen.de>
2017-12-24 11:59:24 +0100

    demo: better style

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 
 import { AppComponent } from './app.component';
 import { TagCloudModule } from 'angular-tag-cloud-module';
@@ -16,6 +17,7 @@ import { TagCloudModule } from 'angular-tag-cloud-module';
     BrowserAnimationsModule,
     MatButtonModule,
     MatExpansionModule,
+    MatTableModule,
     TagCloudModule
   ],
   providers: [],
