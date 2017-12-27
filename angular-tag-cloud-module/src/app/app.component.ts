75fdc558a94728c2203598fb2d103c6775cdf7c5
Danny Koppenhagen <mail@d-koppenhagen.de>
2017-12-27 11:06:57 +0100

    use random data

--- angular-tag-cloud-module/src/app/app.component.ts
+++ angular-tag-cloud-module/src/app/app.component.ts
@@ -15,53 +15,64 @@ export class AppComponent {
     overflow: false,
   };
 
-  data: CloudData[] = [
-    { text: 'Weight-10-link-color', weight: 10, link: 'https://google.com', color: '#ffaaee' },
-    { text: 'Weight-10-link', weight: 10, link: 'https://google.com' },
-    { text: 'Weight-10', weight: 10 },
-    { text: 'Weight-9-link-color', weight: 9, link: 'https://google.com', color: 'red' },
-    { text: 'Weight-9-link', weight: 9, link: 'https://google.com' },
-    { text: 'Weight-9', weight: 9 },
-    { text: 'Weight-8-link-color', weight: 8, link: 'https://google.com', color: 'green' },
-    { text: 'Weight-8-link', weight: 8, link: 'https://google.com' },
-    { text: 'Weight-8', weight: 8 },
-    { text: 'Weight-7-link-color', weight: 7, link: 'https://google.com', color: '#aaa' },
-    { text: 'Weight-7-link-external', weight: 7, link: 'https://google.com', external: true },
-    { text: 'Weight-7', weight: 7 },
-    { text: 'Weight-6-link-color', weight: 6, link: 'https://google.com', color: '#ddd' },
-    { text: 'Weight-6-link', weight: 6, link: 'https://google.com' },
-    { text: 'Weight-6', weight: 6.4 },
-    { text: 'Weight-5-link-color', weight: 5, link: 'https://google.com', color: 'yellow' },
-    { text: 'Weight-5-link', weight: 5, link: 'https://google.com' },
-    { text: 'Weight-5', weight: 5 },
-    { text: 'Weight-4-link-color-external', weight: 4, link: 'https://google.com', color: '#ddd', external: true },
-    { text: 'Weight-4-link', weight: 4, link: 'https://google.com' },
-    { text: 'Weight-4', weight: 4 },
-    { text: 'Weight-3-link-color', weight: 3, link: 'https://google.com', color: '#0f0' },
-    { text: 'Weight-3-link', weight: 3, link: 'https://google.com' },
-    { text: 'Weight-3', weight: 3 },
-    { text: 'Weight-2-link-color', weight: 2, link: 'https://google.com', color: 'olive' },
-    { text: 'Weight-2-link', weight: 2, link: 'https://google.com' },
-    { text: 'Weight-2', weight: 2 },
-    { text: 'Weight-1-link-color', weight: 1, link: 'https://google.com', color: 'purple' },
-    { text: 'Weight-1-link', weight: 1, link: 'https://google.com' },
-    { text: 'Weight-1', weight: 1 }
-  ];
+  data: CloudData[] = this._randomData(50);
 
   dataStrict: CloudData[] = [
     { text: 'Weight-10-link-color', weight: 10, link: 'https://google.com', color: '#ffaaee' }
   ];
 
-  newDateFromObservable() {
-    const changedData$: Observable<CloudData[]> = Observable.of([
-      { text: 'Weight-10-link-color', weight: 8, link: 'https://google.com', color: '#ffaaee' },
-      { text: 'Weight-10-link', weight: 10, link: 'https://google.com' }
-    ]);
+  newDataFromObservable() {
+    const changedData$: Observable<CloudData[]> = Observable.of(this._randomData(50));
     changedData$.subscribe(res => this.data = res);
   }
 
   log(eventType: string, e?: CloudData) {
     console.log(eventType, e);
+  }
+
+  private _randomData(cnt?: number): CloudData[] {
+    if (!cnt) { cnt = 20; }
+
+    const cd: CloudData[] = [];
+
+    for (let i = 0; i < cnt; i++) {
+      let link: string;
+      let color: string;
+      let external: boolean;
+      let weight = 5;
+      let text = '';
+
+      // randomly set link attribute and external
+      if (Math.random() >= 0.5) {
+        link = 'http://example.org';
+        if (Math.random() >= 0.5) { external = true; }
+      }
+
+      // randomly set color attribute
+      if (Math.random() >= 0.5) {
+        color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+      }
+
+      // set random weight
+      weight = Math.floor((Math.random() * 10) + 1);
+
+      text = `weight-${weight}`;
+      if (color) { text += '-color'; }
+      if (link) { text += '-link'; }
+      if (external) { text += '-external'; }
+
+      const el: CloudData = {
+        text: text,
+        weight: weight,
+        color: color,
+        link: link,
+        external: external
+      };
+
+      cd.push(el);
+    }
+
+    return cd;
   }
 }
 
