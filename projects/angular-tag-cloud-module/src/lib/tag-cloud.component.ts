d40d535a072d00ea549e30b189d3f8e2bffe2858
Sheik Althaf <althafmech@outlook.com>
2023-10-26 11:20:25 +0530

    feat: upgrade to angular V15 (#76)
    * feat: upgrade to angular 15
    * fix: handle animation with css variables
    
    BREAKING CHANGES
    - set minimum Angular Version to `15.0.0`

--- projects/angular-tag-cloud-module/src/lib/tag-cloud.component.ts
+++ projects/angular-tag-cloud-module/src/lib/tag-cloud.component.ts
@@ -36,7 +36,7 @@ const DEFAULT_STEP = 1;
 @Component({
   selector: 'angular-tag-cloud, ng-tag-cloud, ngtc',
   template: '',
-  styleUrls: ['./tag-cloud.component.css'],
+  styleUrls: ['./tag-cloud.component.scss'],
   standalone: true,
 })
 export class TagCloudComponent
@@ -514,10 +514,12 @@ export class TagCloudComponent
 
     // delayed appearance
     if (this.options.delay) {
-      wordStyle.animation = 'fadeIn 0.5s';
-      wordStyle.opacity = '0';
-      wordStyle.animationFillMode = 'forwards';
-      wordStyle.animationDelay = `${this.options.delay * index}ms`;
+      wordSpan.classList.add('tag-animation-delay');
+      // add custom css properties
+      wordStyle.setProperty(
+        '--tag-animation-delay',
+        `${this.options.delay * index}ms`,
+      );
     }
 
     // default case: place randomly
