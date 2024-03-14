427c3d0bcb949cc95a8125b080d879f7aaee627e
Danny Koppenhagen <mail@k9n.dev>
2024-03-14 19:05:45 +0100

    chore: remove environment replacements

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,3 @@
-import { enableProdMode } from '@angular/core';
-
-import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
 
 import {
@@ -9,10 +6,6 @@ import {
 } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { bootstrapApplication } from '@angular/platform-browser';
-
-if (environment.production) {
-  enableProdMode();
-}
 
 bootstrapApplication(AppComponent, {
   providers: [provideAnimations(), provideHttpClient(withInterceptorsFromDi())],
