ba90b61581285a01a838400db43814431a432f8d
Danny Koppenhagen <mail@k9n.dev>
2025-08-12 11:37:26 +0200

    fix: migrate to provideZonelessChangeDetection in example app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,17 @@
-import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from "@angular/core";
-import { provideAnimations } from "@angular/platform-browser/animations";
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import {
+  ApplicationConfig,
+  provideZonelessChangeDetection,
+} from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-    providers: [
-        provideExperimentalZonelessChangeDetection(),
-        provideAnimations(),
-        provideHttpClient(withInterceptorsFromDi())
-    ],
-}
+  providers: [
+    provideZonelessChangeDetection(),
+    provideAnimations(),
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
+};
